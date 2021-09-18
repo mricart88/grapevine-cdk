36e3b2870317e5080a6d9e40e9b98e337dc16154
Mario Ricart <marioricart@Marios-MacBook-Pro.local>
2021-09-17 21:38:03 -0700

    Initial commit

---
+++ lib/grapevine-cdk-stack.ts
@@ -0,0 +1,9 @@
+import * as cdk from '@aws-cdk/core';
+
+export class GrapevineCdkStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+  }
+}
